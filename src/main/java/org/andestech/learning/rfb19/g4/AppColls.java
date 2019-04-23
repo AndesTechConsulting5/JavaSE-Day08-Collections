c83014edc0b69ab10025788e9e9385f04982111d
a0z9 <a0z9@rambler.ru>
2019-04-23 17:59:26 +0300

    Collections v.1

---
+++ src/main/java/org/andestech/learning/rfb19/g4/AppColls.java
@@ -0,0 +1,42 @@
+package org.andestech.learning.rfb19.g4;
+
+
+import java.util.*;
+//import org.andestech.learning.rfb19.g4.utils.Utils;
+
+import static org.andestech.learning.rfb19.g4.utils.Utils.genRandomPasswordList;
+import static org.andestech.learning.rfb19.g4.utils.Utils.genRandomIntegerList;
+
+public class AppColls
+{
+
+    public static void main( String[] args )
+    {
+
+        List<Integer> list =
+        new LinkedList<>();
+
+        list.add(1312300);
+        list.add(55550);
+        list.add(-99999);
+        list.add(222);
+        //list.addAll(222,4545,556);
+
+        list.addAll(2,Arrays.asList(new Integer[]{-22,33,44,123123}));
+
+        System.out.println(list);
+
+
+        //list.toString();
+
+        System.out.println(Arrays.asList(1,2,3,4));
+
+        //
+        System.out.println(genRandomIntegerList(10,-100,100));
+        System.out.println(genRandomPasswordList(10, 12));
+
+
+
+
+    }
+}
---
+++ src/test/java/org/andestech/learning/rfb19/g4/AppCollsTest.java
@@ -0,0 +1,20 @@
+package org.andestech.learning.rfb19.g4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple AppColls.
+ */
+public class AppCollsTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
